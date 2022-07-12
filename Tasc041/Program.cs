56222093cc8ae4b559361cbbcb401621327c2979
«Evgenia <evgenia.kopitext@gmail.com>
2022-07-12 21:41:06 +1100

    Tasc041

---
+++ Tasc041/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void PositivNumber (int count)
+{
+    int [] number = new int [count];
+    int positivNumber = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Введите положительное или отрицательное число № {i+1} из {count}: ");
+        number[i] = Convert.ToInt32(Console.ReadLine());
+        positivNumber += number[i] > 0 ? 1:0;
+    }
+    Console.Write("Вы ввели положительных чисел: " + positivNumber);
+}
+int count = 4;
+PositivNumber(count);
